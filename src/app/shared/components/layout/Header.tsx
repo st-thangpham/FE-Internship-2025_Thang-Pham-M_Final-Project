fbdae9ef2ecca1e35d06822a653a35cf8ab28334
st-thangpham <thang.pham@supremetech.vn>
2025-05-07 11:05:19 +0700

    Create demo header for navigation

--- src/app/shared/components/layout/Header.tsx
+++ src/app/shared/components/layout/Header.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '@assets/icons/medium-seeklogo.svg';
 
 export const Header = () => {
+  const getNavLinkClass = ({ isActive }: { isActive: boolean }) =>
+    isActive ? 'nav-link nav-link-active' : 'nav-link';
+
   return (
     <header className="header">
       <div className="container">
-        <p>Header</p>
+        <nav className="navbar">
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </NavLink>
+          <div className="navbar-collapse">
+            <ul className="navbar-nav d-flex">
+              <li className="nav-item">
+                <NavLink to="auth/login" className={getNavLinkClass}>
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="auth/register"
+                  className="nav-link btn btn-primary"
+                >
+                  Register
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </header>
   );
