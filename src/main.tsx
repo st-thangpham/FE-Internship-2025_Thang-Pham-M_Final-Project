2ae729a38fd0e0c11ea629456c5daabccabc1cbd
st-thangpham <thang.pham@supremetech.vn>
2025-05-07 08:27:59 +0700

    Definine API service

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './app/App.tsx';
-import { store } from './redux/store.ts';
+import App from './app/App';
+import { store } from './redux/store';
 import './stylesheet/_style.scss';
 
 createRoot(document.getElementById('root')!).render(
